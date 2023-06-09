26535d0990da1495c1ffad4a040e83be3b01e72e
Chisom Oguibe <chipzstar.dev@googlemail.com>
2023-06-09 14:25:14 +0100

    sync database models for Slack + Zendesk

--- app/db/models.py
+++ app/db/models.py
@@ -23,6 +23,8 @@ class User(Base):
 class Slack(Base):
     __tablename__ = 'Slack'
     id = Column(Integer, primary_key=True, autoincrement=True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
     user_id = Column(String(191, 'utf8mb4_unicode_ci'), nullable=False, unique=True)
     access_token = Column(String(191, 'utf8mb4_unicode_ci'), nullable=False, server_default="")
     team_id = Column(String(191, 'utf8mb4_unicode_ci'), nullable=False, server_default="")
@@ -34,8 +36,9 @@ class Slack(Base):
 
 class Zendesk(Base):
     __tablename__ = 'Zendesk'
-
     id = Column(Integer, primary_key=True, autoincrement=True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
     user_id = Column(String, unique=True)
     access_token = Column(String, nullable=False)
     subdomain = Column(String, unique=True)
