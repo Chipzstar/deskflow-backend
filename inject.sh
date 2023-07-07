5fec1785be450c94b59d029ba797055e0213fbec
Chipzstar <chipzstar.dev@googlemail.com>
2023-07-07 13:36:54 +0100

    minor config script changes

--- inject.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DOPPLER_TOKEN="$(doppler configs tokens create deskflow-docker -p deskflow-backend -c dev_docker --plain --max-age 1m)"
-doppler run \
-  --mount docker-compose.yml \
-  --mount-template doppler-docker-compose.yml \
-  -- cat docker-compose.yml
